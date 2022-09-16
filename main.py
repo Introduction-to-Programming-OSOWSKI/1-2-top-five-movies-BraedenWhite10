f1cb2d51bf185f52a8b2b2d78432a0ab59b6f497
BraedenWhite10 <johndo@mystma.org>
2022-09-16 08:39:28 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 #WRITE YOUR CODE IN THIS FILE
+def top5Movies(a,b,c,d,e):
+    return(a+" "+ b+" "+ c+" "+ d+" "+ e)
+print(top5Movies("1.Pacific Rime" ,"2.Ready Player One", "3.Pacifc Rim 2" ,"4.Saving Private Ryan" ,"5. Shrek"))
